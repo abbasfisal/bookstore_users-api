c46e556ac1d035ef075aad4b910cd5b1facd87e8
abbas <abbasmomeny1994@gmail.com>
2023-03-29 11:16:31 +0330

    Rename user.go to user_dto means data transfer object

--- domain/users/user_dto.go
+++ domain/users/user_dto.go
@@ -16,7 +16,7 @@ type User struct {
 func (u *User) Validate() *errors.RestErr {
 
 	if u.Email = strings.TrimSpace(strings.ToLower(u.Email)); u.Email == "" {
-		
+
 		return errors.NewBadRequestError("invalid email address")
 	}
 	return nil
