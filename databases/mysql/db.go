7940725e6d599ddfb25fab1c4f13956a29fbbb72
abbas <abbasmomeny1994@gmail.com>
2023-03-29 20:45:09 +0330

    implement mysql db

---
+++ databases/mysql/db.go
@@ -0,0 +1,26 @@
+package mysql
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+var (
+	Db *sql.DB
+)
+
+func init() {
+	Db, err := sql.Open("mysql", "root:root@/golang")
+	if err != nil {
+		panic(err.Error())
+	}
+	pErr := Db.Ping()
+	if pErr != nil {
+		log.Fatal(pErr.Error())
+	}
+	fmt.Println("ping database successfully")
+
+	fmt.Println("Connected to db successfully")
+}
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bookstore_users-api/app"
+	_ "bookstore_users-api/databases/mysql"
 )
 
 func main() {
+
 	app.StartApplication()
 }
