126e68d6a1d9df288d736e278731828e52dc6296
abbas <abbasmomeny1994@gmail.com>
2023-03-29 11:05:09 +0330

    create validate method for user

--- domain/users/user.go
+++ domain/users/user.go
@@ -1,9 +1,23 @@
 package users
 
+import (
+	"bookstore_users-api/utils/errors"
+	"strings"
+)
+
 type User struct {
 	Id        int64  `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
 	CreatedAt string `json:"created_at"`
+}
+
+func (u *User) Validate() *errors.RestErr {
+
+	if u.Email = strings.TrimSpace(strings.ToLower(u.Email)); u.Email == "" {
+		
+		return errors.NewBadRequestError("invalid email address")
+	}
+	return nil
 }
